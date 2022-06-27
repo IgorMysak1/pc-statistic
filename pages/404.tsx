81763ff1622cab93362ae952d4ebe5c7d6cc1568
Ihor <igormusak1@gmail.com>
2022-06-27 23:21:55 +0300

    change 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div className="h-full flex items-center justify-center text-9xl">404</div>
+    <div className="h-full flex items-center justify-center text-9xl">
+      (404)
+    </div>
   );
 };
 export default Error;
